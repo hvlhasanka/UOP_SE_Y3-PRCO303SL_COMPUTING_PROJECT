b5fb81cda053608b0f2d39f02547c3e5a8cf9adc
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-02-21 01:57:12 +0530

    GHD Management System - Client-Side - File Structure Setup - v1u2
    - Added initial source files required for operator user interface.

---
+++ Workspace/GHD_Management_System-Web_App-Client-Side/ghd-management-system/src/pages/account/operator-nav-page/health-details-sub-page/health-details-sub-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './health-details-sub-page.css';
+
+const HealthDetailsSubPage = () => {
+
+  return(
+    <div className='health-details-sub-page'>
+
+    </div>
+  );
+
+}
+
+export default HealthDetailsSubPage;
---
+++ Workspace/GHD_Management_System-Web_App-Client-Side/ghd-management-system/src/pages/account/operator-nav-page/operator-nav-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './operator-nav-page.css';
+
+const OperatorNavPage = () => {
+
+  return(
+    <div className='operator-nav-page'>
+
+    </div>
+  );
+
+}
+
+export default OperatorNavPage;
