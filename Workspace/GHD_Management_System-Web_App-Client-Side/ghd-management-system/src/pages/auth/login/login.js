688ef22eb34c9089c1cc9de19d1b992e131a6b88
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-02-21 01:39:26 +0530

    GHD Management System - Client-Side - File Structure Setup - v1
    - Installed react router into the react project.
    - Removed default code blocks.
    - Added initial login page source files.

---
+++ Workspace/GHD_Management_System-Web_App-Client-Side/ghd-management-system/src/pages/auth/login/login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './login.css';
+
+const Login = () => {
+
+  return(
+    <div className='loginPage'>
+      <div className="loginSection">
+    
+      </div>
+      <div className="coverImageSection">
+
+      </div>
+    </div>
+  );
+
+}
+
+export default Login;
