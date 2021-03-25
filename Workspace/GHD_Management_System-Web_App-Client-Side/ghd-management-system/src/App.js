bec44c953b478dcd863ce7077138267806e5d51e
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-03-25 07:05:48 +0530

    Web App - GHD Management System - Login UI - v1u7
    - Improvements to 'Login' user interface.
    - Added page routing through the URL paths using 'react-router-dom'.

--- Workspace/GHD_Management_System-Web_App-Client-Side/ghd-management-system/src/App.js
+++ Workspace/GHD_Management_System-Web_App-Client-Side/ghd-management-system/src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-import Login from './pages/auth/login/login';
-import AdministratorNavPage from './pages/account/administrator-nav-page/administrator-nav-page';
-import OperatorNavPage from './pages/account/operator-nav-page/operator-nav-page';
+import AuthenticationRouter from './router/authentication-router';
 
 function App() {
   return (
     <div className="GHD-Management-System-App">
-      <Login />
-      {/*<AdministratorNavPage />
-      <OperatorNavPage />*/}
+      <AuthenticationRouter />
     </div>
   );
 }
--- Workspace/GHD_Management_System-Web_App-Client-Side/ghd-management-system/src/pages/auth/login/login.js
+++ Workspace/GHD_Management_System-Web_App-Client-Side/ghd-management-system/src/pages/auth/login/login.js
@@ -1,16 +1,15 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { 
   Grid, 
   TextField, 
-  Icon, 
-  InputAdornment, 
-  IconButton, 
+  Button,
 } from '@material-ui/core';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import EmailIcon from '@material-ui/icons/Email';
 import InfoIcon from '@material-ui/icons/Info';
 import LockIcon from '@material-ui/icons/Lock';
+import { Link } from 'react-router-dom';
 
 import './login.css';
 import HeadingOne from '../../../components/heading-one/heading-one.js';
@@ -19,6 +18,14 @@ import logo from '../../../assets/logo/GHD-Management-System-Logo.png';
 import coverImage from '../../../assets/cover-image/cover-image.jpg';
 
 const Login = () => {
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  useEffect(() => {
+    
+  }, []);
 
   return(
     <div className='login'>
@@ -31,86 +38,148 @@ const Login = () => {
         {/* Login Container */}
         <div className="login__login-container">
           <div className="login__inner-login-container">
-            <Grid container justify="center" spacing={2}>
-              <Grid item xs={12}>
-                <HeadingOne className="login__login-heading" headingTitle="Login" />
-              </Grid>
-              <Grid item xs={12}>
+            {/* Login Form */}
+            <form className="login__form">
+              <Grid container justify="center" spacing={2}>
+                <Grid item xs={12}>
+                  <HeadingOne className="login__login-heading" headingTitle="Login" />
+                </Grid>
+                <Grid item xs={12}>
 
-                <div className="login__email-input-text-field">
-                  <Grid container justify="center" spacing={2}>
-                    <Grid item xs={12}>
-                      <Grid container justify="center">
-                        <Grid item xs={1} className="login__left-material-icon">
-                          <EmailIcon />
-                        </Grid>
-                        <Grid item xs={10}>
-                          <HeadingTwo className="login__input-text-field-heading" headingTitle="Email Address" />
-                        </Grid>
-                        <Grid item xs={1} className="login__right-material-icon" >
-                          <InfoIcon />
+                  <div className="login__email-input-text-field">
+                    <Grid container justify="center" spacing={2}>
+                      <Grid item xs={12}>
+                        <Grid container justify="center">
+                          <Grid item xs={1} className="login__left-material-icon">
+                            <EmailIcon />
+                          </Grid>
+                          <Grid item xs={10}>
+                            <HeadingTwo className="login__input-text-field-heading" headingTitle="Email Address" />
+                          </Grid>
+                          <Grid item xs={1} className="login__right-material-icon" >
+                            <button>
+                              <InfoIcon />
+                            </button>
+                          </Grid>
                         </Grid>
                       </Grid>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField 
-                        id="text-field" 
-                        className="text-field login__email-address-text-field" 
-                        variant="outlined" 
-                        value=""
-                      />
-                    </Grid>
-                  </Grid>
-                </div>
-
-              </Grid>
-              <Grid item xs={12}>
-
-                <div className="login__password-input-text-field">
-                  <Grid container justify="center" spacing={2}>
-                    <Grid item xs={12}>
-                      <Grid container justify="center">
-                        <Grid item xs={1} className="login__left-material-icon">
-                          <LockIcon />
-                        </Grid>
-                        <Grid item xs={10}>
-                          <HeadingTwo className="login__input-text-field-heading" headingTitle="Password" />
-                        </Grid>
-                        <Grid item xs={1} className="login__right-material-icon" >
-                          <InfoIcon />
-                        </Grid>
+                      <Grid item xs={12}>
+                        <TextField 
+                          id="text-field" 
+                          className="text-field login__email-address-text-field" 
+                          variant="outlined" 
+                          value={emailAddress}
+                          onChange={(textField) => setEmailAddress(textField.target.value)}
+                        />
                       </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                      <TextField 
-                        id="text-field-password" 
-                        className="text-field login__password-text-field" 
-                        variant="outlined" 
-                        value=""
-                      />
+                  </div>
+
+                </Grid>
+                <Grid item xs={12}>
+
+                  <div className="login__password-input-text-field">
+                    <Grid container justify="center" spacing={2}>
+                      <Grid item xs={12}>
+                        <Grid container justify="center">
+                          <Grid item xs={1} className="login__left-material-icon">
+                            <LockIcon />
+                          </Grid>
+                          <Grid item xs={10}>
+                            <HeadingTwo className="login__input-text-field-heading" headingTitle="Password" />
+                          </Grid>
+                          <Grid item xs={1} className="login__right-material-icon" >
+                            <button>
+                              <InfoIcon />
+                            </button>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField 
+                          id="text-field-password" 
+                          className="text-field login__password-text-field" 
+                          variant="outlined"
+                          type={showPassword === true ? "text" : "password"} 
+                          value={password}
+                          onChange={(textField) => setPassword(textField.target.value)}
+                        />
+                        <button type="button" onClick={() => setShowPassword(!showPassword)}>
+                          {showPassword === true ? <VisibilityOff /> : <Visibility />}
+                        </button>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Button className="login__forgot-password-button" style={{textTransform: 'none'}}>
+                          <b>Forgot password?</b>
+                        </Button>
+                      </Grid>
                     </Grid>
-                  </Grid>
-                </div>
+                  </div>
 
-              </Grid>
-              <Grid item xs={12}>
-                
-                <div className="login__login-button">
-                     
-                </div>
+                </Grid>
+                <Grid item xs={12}>
+                  
+                  <div className="login__login-button-container">
+                    <Button 
+                      variant="outlined" 
+                      color="primary" 
+                      className="login__login-button" 
+                      disabled={emailAddress.length === 0 || password.length === 0}>
+                      <b>LOGIN</b>
+                    </Button>
+                  </div>
 
+                </Grid>
+                <Grid item xs={12}>
+                  
+                  <div className="login__no-account">
+                    <p>Don't have an account?
+                      <Button variant="outlined" className="login__forgot-password-button" style={{textTransform: 'none'}}>
+                        <b>Submit Report</b>
+                      </Button>
+                    </p>
+                  </div>
+
+                </Grid>
+                <Grid item xs={12}>
+                  
+                  <div className="login__footer-row">
+                    <Grid container justify="center" spacing={2}>
+                      <Grid item xs={3}>
+                      <Button variant="outlined" className="login__report_button">
+                        <b>REPORT</b>
+                      </Button>
+                      </Grid>
+                      <Grid item xs={3}>
+                        
+                      </Grid>
+                      <Grid item xs={3}>
+                        
+                      </Grid>
+                      <Grid item xs={3}>
+                        <Button variant="outlined" className="login__about-button">
+                          <b>ABOUT</b>
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </div>
+
+                </Grid>
               </Grid>
-            </Grid>
+            </form>
             
           </div>
         </div>
 
       </div>
+
+      {/* Cover Image Container */}
       <div className="login__cover-image-section">
-        <div className="login__cover-image-container">
+        <Link to={""} className="login__cover-image-container">
           <img src={coverImage} alt="Cover Image" className="login__cover-image" />
-        </div>
+        </Link>
       </div>
+
     </div>
   );
 
