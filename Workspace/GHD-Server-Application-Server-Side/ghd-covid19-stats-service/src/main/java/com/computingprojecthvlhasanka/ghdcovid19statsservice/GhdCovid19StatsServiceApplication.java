970d1f311359ff16f703137b374fca0edc4f36d5
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-04-28 01:32:11 +0530

    GHD Covid19 Stats Service - Initial Project Files
    - Added initial source files of the 'ghd-covid19-stats-service' microservice app.

---
+++ Workspace/GHD-Server-Application-Server-Side/ghd-covid19-stats-service/src/main/java/com/computingprojecthvlhasanka/ghdcovid19statsservice/GhdCovid19StatsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.computingprojecthvlhasanka.ghdcovid19statsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhdCovid19StatsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhdCovid19StatsServiceApplication.class, args);
+	}
+
+}
---
+++ Workspace/GHD-Server-Application-Server-Side/ghd-covid19-stats-service/src/test/java/com/computingprojecthvlhasanka/ghdcovid19statsservice/GhdCovid19StatsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computingprojecthvlhasanka.ghdcovid19statsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhdCovid19StatsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
