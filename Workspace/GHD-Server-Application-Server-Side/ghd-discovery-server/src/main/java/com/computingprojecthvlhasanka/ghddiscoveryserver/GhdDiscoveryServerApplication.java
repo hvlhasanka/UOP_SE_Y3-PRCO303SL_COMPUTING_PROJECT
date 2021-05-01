00cf3a63caaf3b00bb6da325e49f5c2fc7fb5cf7
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-05-02 00:19:21 +0530

    GHD Discovery Server - Added Initial Source Files
    - Added initial sources files of GHD Discovery Server.

---
+++ Workspace/GHD-Server-Application-Server-Side/ghd-discovery-server/src/main/java/com/computingprojecthvlhasanka/ghddiscoveryserver/GhdDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.computingprojecthvlhasanka.ghddiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhdDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhdDiscoveryServerApplication.class, args);
+	}
+
+}
