100adcb5d9283a43077574bc205964150f81a488
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-05-07 10:49:39 +0530

    Mobile App - Health Details Screen - v1u2
    - Added user interface to render when api call has failed.

--- Workspace/GHD_COVID19-Mobile_App-Client-Side/ghd_covid19/src/screens/public-screens/health-details-tab/health-details-tab.screen.js
+++ Workspace/GHD_COVID19-Mobile_App-Client-Side/ghd_covid19/src/screens/public-screens/health-details-tab/health-details-tab.screen.js
@@ -5,6 +5,7 @@ import {
   ScrollView
 } from 'react-native';
 import { Card, Title, Button, Paragraph } from 'react-native-paper';
+import MaterialIconsIcon from 'react-native-vector-icons/MaterialIcons';
 
 import styles from './health-details-tab.style';
 import HealthNewsService from '../../../services/public/health-news-service';
@@ -12,18 +13,20 @@ import HealthNewsService from '../../../services/public/health-news-service';
 
 const HealthDetailsTabScreen = ({ navigation }) => {
 
+  const [healthNewsStatus, setHealthNewsStatus] = useState();
   const [healthNews, setHealthNews] = useState([]);
-
+  
   const retrieveHealthNewsDetails = async () => {
     try {
       const responseHealthNewsDetails = await HealthNewsService
         .getHealthNewsDetails()
         .then(response => {
+          setHealthNewsStatus(response.data.status);
           setHealthNews(response.data.articles);
         });
     } catch (error) {
       console.log(error);
-      // TODO: SHOW LOAD ERROR MESSAGE
+      setHealthNewsStatus("error");
     }
   }
 
@@ -31,6 +34,7 @@ const HealthDetailsTabScreen = ({ navigation }) => {
     const unsubscribe = navigation.addListener('focus', () => {
       retrieveHealthNewsDetails().then(() => {
         // AFTER RETRIEVING API DATA
+        console.log(healthNews);
       });
     });
     return unsubscribe;
@@ -48,29 +52,45 @@ const HealthDetailsTabScreen = ({ navigation }) => {
           <Text style={styles.blockHeading}>
             Public Health News
           </Text>
-          <ScrollView horizontal={true}
-          showsHorizontalScrollIndicator={false}>
-            <View style={styles.healthNewsBlock}>
-              {
-                  healthNews.map((listOfHealthNews, key) => {
-                    return (
-                      <View key={ listOfHealthNews.source.name } key={ listOfHealthNews.source.name }style={styles.healthNewsBlockItem}>
-                        <Card style={[styles.healthNewsCard, styles.blockShadow]}>
-                          <Card.Cover source={{ uri: listOfHealthNews.urlToImage }} />
-                          <Card.Content>
-                            <Title>{ listOfHealthNews.title }</Title>
-                            <Paragraph>{ listOfHealthNews.description }</Paragraph>
-                          </Card.Content>
-                          <Card.Actions>
-                            <Button onPress={() => navigation.navigate("PublicHealthNews", { newsDetails: listOfHealthNews })}>READ MORE</Button>
-                          </Card.Actions>
-                        </Card>
-                      </View>
-                    );
-                  })
-                }
-            </View>
-          </ScrollView>
+          { 
+            healthNewsStatus === "ok" ? 
+              <ScrollView horizontal={true}
+                showsHorizontalScrollIndicator={false}
+              >
+              <View style={styles.healthNewsBlock}>
+                {
+                    healthNews.map((listOfHealthNews, key) => {
+                      return (
+                        <View key={ listOfHealthNews.source.name } key={ listOfHealthNews.source.name }style={styles.healthNewsBlockItem}>
+                          <Card style={[styles.healthNewsCard, styles.blockShadow]}>
+                            <Card.Cover source={{ uri: listOfHealthNews.urlToImage }} />
+                            <Card.Content>
+                              <Title>{ listOfHealthNews.title }</Title>
+                              <Paragraph>{ listOfHealthNews.description }</Paragraph>
+                            </Card.Content>
+                            <Card.Actions>
+                              <Button onPress={() => navigation.navigate("PublicHealthNews", { newsDetails: listOfHealthNews })}>READ MORE</Button>
+                            </Card.Actions>
+                          </Card>
+                        </View>
+                      );
+                    })
+                  }
+              </View>
+            </ScrollView>
+          : 
+            <View style={styles.loadErrorBlock}>
+              <MaterialIconsIcon 
+                  name="error-outline" 
+                  size={35} 
+                  color="#FB3151"
+                  onPress={() => navigation.openDrawer()}
+              ></MaterialIconsIcon>
+              <Text style={styles.loadErrorText}>
+                ERROR: Unable to load
+              </Text>
+            </View> 
+          }
         </View>
       </ScrollView>
     </View>
--- Workspace/GHD_COVID19-Mobile_App-Client-Side/ghd_covid19/src/screens/public-screens/health-details-tab/health-details-tab.style.js
+++ Workspace/GHD_COVID19-Mobile_App-Client-Side/ghd_covid19/src/screens/public-screens/health-details-tab/health-details-tab.style.js
@@ -24,7 +24,8 @@ const styles =  StyleSheet.create({
     paddingBottom: 10,
     width: "90%",
     backgroundColor: "#ffffff",
-    borderRadius: 10
+    borderRadius: 10,
+    minHeight: 190
   },
   healthNewsBlock: {
     flexDirection: "row",
@@ -46,6 +47,25 @@ const styles =  StyleSheet.create({
   },
   healthNewsCard: {
     width: 260
+  },
+  loadErrorBlock: {
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 7,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 3,
+    borderColor: "#FB3151",
+    backgroundColor: "#FFEBED"
+  },
+  loadErrorText: {
+    fontSize: 15,
+    letterSpacing: 1,
+    fontWeight: "bold",
+    marginLeft: 10,
+    color: "#FB3151"
   },
   blockShadow: {
     shadowColor: "#000",
