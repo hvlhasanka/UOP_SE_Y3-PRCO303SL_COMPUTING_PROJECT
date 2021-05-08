32fe758b59158dfb54ec8c06699f4244d04ff52e
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-05-08 18:03:23 +0530

    Mobile App - Login Screen - v1u1
    - Improved user interface on the 'Login Screen'.

--- Workspace/GHD_COVID19-Mobile_App-Client-Side/ghd_covid19/src/screens/auth/login/login.screen.js
+++ Workspace/GHD_COVID19-Mobile_App-Client-Side/ghd_covid19/src/screens/auth/login/login.screen.js
@@ -2,16 +2,106 @@ import React from 'react';
 import {
   Text,
   View,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+  Image
 } from 'react-native';
-
+import MaterialCommunityIconsIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import { Button } from 'react-native-paper';
 import styles from './login.style';
 
+
+const loginCoverImage = require("../../../assets/images/login-screen/cover-image.jpg");
+
 const LoginScreen = () => {
+
   return (
     <View style={styles.container}>
-      <Text style={{ color: 'black' }}>
-        Login Screen
-      </Text>
+      <StatusBar backgroundColor="#4E3AC2" />
+      <View style={styles.loginHeader}>
+        <TouchableOpacity style={styles.closeLoginButton} onPress={() => {}}>
+          <MaterialCommunityIconsIcon
+            name="close"
+            color= "#4E3AC2"
+            size={30}
+          />
+        </TouchableOpacity>
+        <Image
+          style={styles.loginCoverImage}
+          source={loginCoverImage}
+        />
+      </View>
+      <View style={[styles.loginFooter, styles.blockShadow]}>
+        <Text style={styles.footerHeading}>
+          LOGIN
+        </Text>
+        <Text style={styles.textBoxHeading}>
+          Email Address
+        </Text>
+        <View style={styles.formInputRow}>
+          <MaterialCommunityIconsIcon
+            name="email-outline"
+            color= "#fff"
+            size={20}
+            style={styles.inputLeftIcon}
+          />
+          <TextInput
+            style={styles.textInput}
+            autoCapitalize="none"
+            placeholder="Account Email"
+          />
+          {/* <FeatherIcon 
+            size={20}
+            name="check-circle"
+            color="#0FEEA5"
+            style={styles.inputRightIcon}
+          /> */}
+        </View>
+        <Text style={styles.textBoxHeading}>
+          Password
+        </Text>
+        <View style={styles.formInputRow}>
+          <FontAwesomeIcon
+            name="lock"
+            color= "#fff"
+            size={20}
+            style={styles.inputLeftIcon}
+          />
+          <TextInput
+            style={styles.textInput}
+            autoCapitalize="none"
+            placeholder="Account Password"
+            secureTextEntry={true}
+          />
+          <FeatherIcon 
+            size={20}
+            name="eye-off"
+            color="#02EEFF"
+            style={styles.inputRightIcon}
+          />
+          <TouchableOpacity style={styles.forgotPasswordBlock}>
+            <Text style={styles.forgotPasswordText}>
+              Forgot Password?
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <Button mode="contained" style={styles.loginButton} onPress={() => {}}>
+          LOGIN
+        </Button>
+        <View style={styles.signUpTextBlock}>
+          <Text style={styles.signUpText}>
+            Don't have an account?
+          </Text>
+          <TouchableOpacity style={styles.signUpButton}>
+            <Text style={styles.signUpButtonText}>
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
--- Workspace/GHD_COVID19-Mobile_App-Client-Side/ghd_covid19/src/screens/auth/login/login.style.js
+++ Workspace/GHD_COVID19-Mobile_App-Client-Side/ghd_covid19/src/screens/auth/login/login.style.js
@@ -3,8 +3,134 @@ import { StyleSheet } from 'react-native';
 const styles =  StyleSheet.create({
   container: { 
     flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center' 
+    backgroundColor: "#fff"
+  },
+  loginHeader: {
+    flex: 1,
+    paddingTop: 30,
+    paddingBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  closeLoginButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 23,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: 15,
+    left: 10,
+    borderWidth: 2,
+    borderColor: "#4E3AC2"
+  },
+  loginCoverImage: {
+    width: "60%",
+    resizeMode: "contain"
+  },
+  loginFooter: {
+    flex: 3,
+    backgroundColor: "#4E3AC2",
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    paddingVertical: 20,
+    paddingHorizontal: 20
+  },
+  footerHeading: {
+    textAlign: "center",
+    fontSize: 24,
+    color: "#fff",
+    fontWeight: "bold",
+    letterSpacing: 1,
+    marginBottom: 10,
+    letterSpacing: 2
+  },
+  textBoxHeading: {
+    fontSize: 20,
+    color: "#fff",
+    letterSpacing: 1.5,
+    marginTop: 10
+  },
+  formContainer: {
+    flexDirection: "row"
+  },
+  formInputRow: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  inputLeftIcon: {
+    position: "absolute",
+    top: 15,
+    left: 10
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: "#5F44F6",
+    borderRadius: 10,
+    paddingLeft: 40,
+    paddingRight: 40
+  },
+  inputRightIcon: {
+    position: "absolute",
+    right: 10,
+    top:15
+  },
+  forgotPasswordBlock: {
+    width: "55%",
+    height: 30,
+    backgroundColor: "#5039D7",
+    borderRadius: 20,
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    top: 60
+  },
+  forgotPasswordText: {
+    fontSize: 14,
+    letterSpacing: 1,
+    color: "#fff",
+    textAlign: "center",
+    paddingTop: 5,
+    fontWeight: "bold"
+  },
+  loginButton: {
+    backgroundColor: "#1F0F7F",
+    borderRadius: 10,
+    marginTop: "30%"
+  },
+  signUpTextBlock: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+    marginTop: "10%"
+  },
+  signUpText: {
+    color: "#fff",
+    paddingRight: 5,
+    fontSize: 15,
+    letterSpacing: 0.8
+  },
+  signUpButton: {
+    padding: 10,
+    backgroundColor: "#5039D7",
+    borderRadius: 10
+  },
+  signUpButtonText: {
+    letterSpacing: 1,
+    fontWeight: "bold",
+    color: "#fff"
+  },
+  blockShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+    elevation: 24,
   }
 });
 
