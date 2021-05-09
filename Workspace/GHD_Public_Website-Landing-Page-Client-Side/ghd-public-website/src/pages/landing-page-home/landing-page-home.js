80334f1ddad0158cc4edcaffd3bc09e649f8dd36
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2021-05-09 22:55:26 +0530

    Landing Page - Landing Page Home Page - v1u1
    - Improved user interface in the 'Landing Page Home Page'.
    - Added responsiveness to the 'Landing Page Home Page'.

--- Workspace/GHD_Public_Website-Landing-Page-Client-Side/ghd-public-website/src/pages/landing-page-home/landing-page-home.js
+++ Workspace/GHD_Public_Website-Landing-Page-Client-Side/ghd-public-website/src/pages/landing-page-home/landing-page-home.js
@@ -2,11 +2,13 @@
  * GHD Public Website - React Web App
  * PAGE | END USER - PUBLIC | Landing Page Home Page
  */
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   Grid,
-  Button
+  Button,
+  Snackbar
 } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 
 import './landing-page-home.css';
 import ghdCovid19Logo from '../../assets/images/logo/GHD-COVID19-Logo.png';
@@ -14,7 +16,25 @@ import uiImageOne from '../../assets/images/landing-page-home-page/ui-image-one.
 import uiImageTwo from '../../assets/images/landing-page-home-page/ui-image-two.jpg';
 import uiImageThree from '../../assets/images/landing-page-home-page/ui-image-three.jpg';
 
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 const LandingPageHomePage = () => {
+
+  const [snackbarEnabled, setSnackbarEnabled] = useState(false);
+
+  const enableSnackBar = () => {
+    setSnackbarEnabled(true);
+  };
+
+  const closeSnackBar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarEnabled(false);
+  };
 
   return(
   <div className="landing-page-home">
@@ -28,7 +48,7 @@ const LandingPageHomePage = () => {
     <div className="body-container">
       <div className="content-container">
       <Grid container justify="center" spacing={2}> 
-        <Grid item xs={5}>
+        <Grid item xs={12} lg={5}>
           <Grid container justify="center" spacing={2} className="left-grid-container">
             <Grid item xs={12}>
               <div className="text-heading-block">
@@ -49,8 +69,10 @@ const LandingPageHomePage = () => {
                   textTransform: 'none', 
                   fontWeight: 'bold',
                   borderWidth: 3,
-                  borderRadius: 10
-                }} className="button">
+                  borderRadius: 10,
+                  borderColor: '#236C9C',
+                  color: '#205476'
+                }} className="button" onClick={enableSnackBar}>
                   Download App | GOOGLE ANDROID
                 </Button>
               </div>
@@ -59,18 +81,25 @@ const LandingPageHomePage = () => {
                   textTransform: 'none', 
                   fontWeight: 'bold',
                   borderWidth: 3,
-                  borderRadius: 10
-                }} className="button">
+                  borderRadius: 10,
+                  borderColor: '#236C9C',
+                  color: '#205476'
+                }} className="button" onClick={enableSnackBar}>
                   Download App | APPLE iOS
                 </Button>
               </div>
+              <Snackbar open={snackbarEnabled} autoHideDuration={2000} onClose={closeSnackBar}>
+                <Alert onClose={closeSnackBar} severity="info">
+                  Coming Soon
+                </Alert>
+              </Snackbar>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={7}>
+        <Grid item xs={12} lg={7}>
           <Grid container justify="center" spacing={2} className="right-grid-container">
             <Grid item xs={12}>
-              <Grid container justify="center" spacing={2}>
+              <Grid container justify="center" spacing={0}>
                 <Grid item xs={4}>
                   <div className="ui-image-tile-block tile-one">
                     <img src={uiImageOne} alt="GHD COVID19 UI Image One" className="ui-image" />
